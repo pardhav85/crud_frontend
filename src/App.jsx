f72016120cd99fa5de8df0924d3397d51bcdc403
M.Pardhava Praneeth <pardhav.2006@gmail.com>
2025-08-21 12:16:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const BASE_URL = 'http://localhost:8081';
     setIsEditing(true);
   };
 
-  /*
+  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
       await axios.delete(`${BASE_URL}/delete/${id}`);
@@ -55,7 +55,7 @@ const BASE_URL = 'http://localhost:8081';
       fetchProducts();
     }
   };
-  */
+  
 
   useEffect(() => {
     fetchProducts();
@@ -168,14 +168,14 @@ const BASE_URL = 'http://localhost:8081';
                 >
                   Edit
                 </button>
-                {/*
+                {
                 <button
                   className="btn btn-danger btn-sm"
                   onClick={() => deleteProduct(p.id)}
                 >
                   Delete
                 </button>
-                */}
+                }
               </td>
             </tr>
           ))}
